fd70f7eb0187c23687bd95c531a9a01b95def6cc
Rajat Chaudhary <chaudharyrajat5459@gmail.com>
2026-07-01 13:19:45 +0530

    Update and rename notes.json to models/note.js

---
+++ models/note.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const noteSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
+    note: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("Note", noteSchema);
